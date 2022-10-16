a668eb9226c149c6576a6cf09bbd26bd1b6e6df5
KamilSzus <69347040+KamilSzus@users.noreply.github.com>
2022-10-16 14:14:52 +0200

    add simle login and update kotlin, add firebase dependencies

---
+++ app/src/main/java/pl/grzybiarze/gatherer/Activity/LoginActivity.kt
@@ -0,0 +1,48 @@
+package pl.grzybiarze.gatherer.Activity
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import pl.grzybiarze.gatherer.R
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val username = findViewById<EditText>(R.id.username)
+        val password = findViewById<EditText>(R.id.password)
+        val submit = findViewById<Button>(R.id.submit)
+        auth = Firebase.auth
+
+        submit.setOnClickListener {
+            val usernameText = username.text.toString();
+            val passwordText = password.text.toString();
+            signIn(usernameText,passwordText);
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
