dbfbdcfbf4fdfe21e157ce2b19e2b4a4b6727a67
adam-pietrasik <adam.pietrasik90@gmail.com>
2022-10-17 21:04:18 +0200

    Added sign in button funcionality to move from welcome page to sign in activity

--- app/src/main/java/pl/grzybiarze/gatherer/MainActivity.kt
+++ app/src/main/java/pl/grzybiarze/gatherer/MainActivity.kt
@@ -1,11 +1,27 @@
 package pl.grzybiarze.gatherer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import pl.grzybiarze.gatherer.activity.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val signInButton = findViewById<Button>(R.id.sign_in_button)
+            .setOnClickListener {
+                Intent(this, LoginActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+
+        // TODO: Create registration activity and pinup it to this button
+        val signUpButton = findViewById<Button>(R.id.sign_up_button)
     }
 }
