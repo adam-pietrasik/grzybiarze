2751c744c67ccd0fde693f041728df93b2431190
KamilSzus <69347040+KamilSzus@users.noreply.github.com>
2022-11-30 22:39:56 +0100

    user front

--- app/src/main/java/pl/grzybiarze/gatherer/activity/UserActivity.kt
+++ app/src/main/java/pl/grzybiarze/gatherer/activity/UserActivity.kt
@@ -3,30 +3,41 @@ package pl.grzybiarze.gatherer.activity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.EditText
+import android.widget.GridView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import pl.grzybiarze.gatherer.R
 import pl.grzybiarze.gatherer.adapters.StatisticItemGridView
 import pl.grzybiarze.gatherer.data.StatisticItemModal
-import pl.grzybiarze.gatherer.dialogs.ForgotPasswordDialog
 
 class UserActivity : AppCompatActivity(){
 
     private val TAG = "UserActivity"
     private lateinit var auth: FirebaseAuth
-    lateinit var statisticGridView: StatisticItemGridView
-    lateinit var statisticItemModal: List<StatisticItemModal>
+
+    private lateinit var statisticGridView: GridView
+    private lateinit var statisticItemModal: List<StatisticItemModal>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView()
+        setContentView(R.layout.activity_user)
+        statisticGridView = findViewById(R.id.stat_view)
+        statisticItemModal = ArrayList<StatisticItemModal>()
+
+        statisticItemModal = statisticItemModal + StatisticItemModal(1,"Wróbel")
 
         val submit = findViewById<Button>(R.id.submit)
         //auth = Firebase.auth
+
+        val courseAdapter = StatisticItemGridView(statisticList = statisticItemModal, this@UserActivity)
+
+
+        statisticGridView.adapter = courseAdapter
+
+        // on below line we are setting adapter to our grid view.
+        //statisticGridView.adapter = courseAdapter
 
         submit.setOnClickListener {
             getUsers()
