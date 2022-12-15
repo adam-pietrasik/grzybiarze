ec37236b94a553ab8ec87127b1894c6766210145
BarFil <fbaranowski99@gmail.com>
2022-12-15 20:37:39 +0100

    coś tam zrobione

--- app/src/main/java/pl/grzybiarze/gatherer/activity/MenuActivity.kt
+++ app/src/main/java/pl/grzybiarze/gatherer/activity/MenuActivity.kt
@@ -1,6 +1,7 @@
 package pl.grzybiarze.gatherer.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -20,9 +21,26 @@ class MenuActivity : AppCompatActivity() {
         var navView : NavigationView = findViewById(R.id.nav_view)
 
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+
         drawerLayout.addDrawerListener(toggle)
+
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+
+                R.id.nav_profile -> Toast.makeText(applicationContext,"Profil",Toast.LENGTH_SHORT).show()
+                R.id.nav_friends -> Toast.makeText(applicationContext,"Znajomi",Toast.LENGTH_SHORT).show()
+                R.id.nav_mapa -> Toast.makeText(applicationContext,"Mapa",Toast.LENGTH_SHORT).show()
+                R.id.nav_pogoda -> Toast.makeText(applicationContext,"Pogoda",Toast.LENGTH_SHORT).show()
+                R.id.nav_aparat -> Toast.makeText(applicationContext,"Aparat",Toast.LENGTH_SHORT).show()
+                R.id.nav_ustawienia -> Toast.makeText(applicationContext,"Ustawienia",Toast.LENGTH_SHORT).show()
+                R.id.nav_about -> Toast.makeText(applicationContext,"O Aplikacji",Toast.LENGTH_SHORT).show()
+
+            }
+            true
+        }
 
     }
 
